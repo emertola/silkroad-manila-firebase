4292d7a97003315dfde07575e5196b22118bae38
Erwin Mertola <emertola@silkroadtech.com>
2018-12-18 20:27:21 +0800

    Firebase Authentication setup

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,7 +1,8 @@
 import firebase from "firebase/app";
+import "firebase/auth";
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
+  apiKey: "AIzaSyD5r8rqKU7iKkLaEU6EBdwxDDkaVRvi06A",
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
@@ -12,7 +13,20 @@ const config = {
 class Firebase {
   constructor() {
     firebase.initializeApp(config);
+    this.auth = firebase.auth();
   }
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password)
 }
 
 export default Firebase;
