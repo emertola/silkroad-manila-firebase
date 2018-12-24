02b73e28fb9188cc78e0d4ba44334fe81e62cfd0
Erwin Mertola <emertola@silkroadtech.com>
2018-12-24 15:06:13 +0800

    Updated SignUp with Context API

--- src/components/authentication/SignUp.js
+++ src/components/authentication/SignUp.js
@@ -1,69 +1,112 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import { withFirebase } from "../firebase";
 
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
-
-class SignUpForm extends Component {
-    constructor() {
-        super()
-    }
-
-    render() {
-        return (
-            <div>Test</div>
-        )
-    }
-}
-
-
-const styles = theme => ({
-    main: {
-      width: 'auto',
-      display: 'block', // Fix IE 11 issue.
-      marginLeft: theme.spacing.unit * 3,
-      marginRight: theme.spacing.unit * 3,
-      [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-        width: 400,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-      },
-    },
-    paper: {
-      marginTop: theme.spacing.unit * 8,
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-    },
-    avatar: {
-      margin: theme.spacing.unit,
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing.unit,
-    },
-    submit: {
-      marginTop: theme.spacing.unit * 3,
-      backgroundColor: "#2196f3",
-      '&:hover': {
-          backgroundColor: "#1e88e5",
-      },
-      color: "#fff"
-    },
-  });
-
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 const SignUp = props => {
-    const { classes } = props;
-  
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  );
+};
+
+const styles = theme => ({
+  main: {
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing.unit
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3,
+    backgroundColor: "#2196f3",
+    "&:hover": {
+      backgroundColor: "#1e88e5"
+    },
+    color: "#fff"
+  }
+});
+
+const INITIAL_STATE = {
+  email: "",
+  password: "",
+  passwordConf: "",
+  error: null
+};
+
+class SignUpFormBase extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ...INITIAL_STATE
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onInputChange = e => {
+    const { name } = e.target;
+    this.setState({
+      [name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    this.props.firebase
+      .doCreateUserWithEmailAndPassword(email, password)
+      .then(authUser => this.setState({ ...INITIAL_STATE }))
+      .catch(error => this.setState({ error }));
+  };
+
+  render() {
+    console.log(this.props);
+    const { classes } = this.props;
+    const { email, password, passwordConf, error } = this.state;
+    const isEmailValid = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
+      email
+    );
+
+    // if invalid, disable the submit button
+    const isInvalid =
+      password === "" ||
+      passwordConf === "" ||
+      password !== passwordConf ||
+      !isEmailValid;
+
     return (
       <main className={classes.main}>
         <CssBaseline />
@@ -71,38 +114,59 @@ const SignUp = props => {
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <form className={classes.form}>
+          <form className={classes.form} onSubmit={this.onSubmit}>
             <FormControl margin="normal" required fullWidth>
               <InputLabel htmlFor="email">Email Address</InputLabel>
-              <Input id="email" name="email" autoComplete="email" autoFocus />
+              <Input
+                id="email"
+                name="email"
+                onChange={this.onInputChange}
+                value={email}
+              />
             </FormControl>
             <FormControl margin="normal" required fullWidth>
               <InputLabel htmlFor="password">Password</InputLabel>
-              <Input name="password" type="password" id="password" autoComplete="current-password" />
+              <Input
+                name="password"
+                type="password"
+                id="password"
+                onChange={this.onInputChange}
+                value={password}
+              />
             </FormControl>
             <FormControl margin="normal" required fullWidth>
               <InputLabel htmlFor="passwordConf">Confirm Password</InputLabel>
-              <Input name="passwordConf" type="password" id="passwordConf" autoComplete="password-confirmation" />
+              <Input
+                name="passwordConf"
+                type="password"
+                id="passwordConf"
+                onChange={this.onInputChange}
+                value={passwordConf}
+              />
             </FormControl>
             <Button
               type="submit"
               fullWidth
               variant="contained"
               className={classes.submit}
+              disabled={isInvalid}
             >
               Sign up
             </Button>
+            {error && <p>{error.message}</p>}
           </form>
         </Paper>
-        
       </main>
     );
   }
-  
-  SignUp.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
+}
 
-  export default withStyles(styles)(SignUp)
+SignUpFormBase.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 
-  export {SignUpForm}
+const SignUpForm = withFirebase(withStyles(styles)(SignUpFormBase))
+
+export default SignUp;
+
+export { SignUpForm };
