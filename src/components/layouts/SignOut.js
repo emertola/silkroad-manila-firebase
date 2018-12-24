7f659de7545053d98bcdb62fcfde2ed794064870
Erwin Mertola <emertola@silkroadtech.com>
2018-12-24 22:39:00 +0800

    Added Sign out

---
+++ src/components/layouts/SignOut.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MenuItem from "@material-ui/core/MenuItem";
+import {withFirebase} from '../firebase'
+
+const SignOut = ({firebase}) => {
+    return (
+        <MenuItem onClick={firebase.doSignOut}>Sign out</MenuItem>
+    )
+}
+
+export default withFirebase(SignOut)
