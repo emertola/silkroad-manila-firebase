d421ba4dd1b87e9e45fb12c614aa7435db26b647
Erwin Mertola <emertola@silkroadtech.com>
2018-12-27 22:51:15 +0800

    Firebase Realtime Database set up

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 
 const config = {
   apiKey: "AIzaSyD5r8rqKU7iKkLaEU6EBdwxDDkaVRvi06A",
@@ -14,6 +15,7 @@ class Firebase {
   constructor() {
     firebase.initializeApp(config);
     this.auth = firebase.auth();
+    this.db = firebase.database();
   }
 
   doCreateUserWithEmailAndPassword = (email, password) =>
@@ -26,7 +28,13 @@ class Firebase {
 
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
 
-  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password)
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  // User API
+
+  user = uid => this.db.ref(`users/${uid}`);
+
+  users = () => this.db.ref("users");
 }
 
 export default Firebase;
